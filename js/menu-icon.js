ac23aac7b2f8394077d6954f2af381a1692d8ef8
Alex Zaloznyi <106338161+alex-zal-19@users.noreply.github.com>
2022-09-28 09:49:34 +0300

    Delete js directory

--- js/menu-icon.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.menu-icon-wrapper').onclick = function(){
-    document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
-    document.querySelector('.navigation-list').classList.toggle('navigation-list--visible');
-    document.querySelector('.logo').classList.toggle('logo--visible');
-};
